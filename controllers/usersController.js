d1817d00325019a8c525ccb1608fa4f82f7f0a33
Riyaz-Github-Acc <riyazofficial0@gmail.com>
2023-07-06 10:43:44 +0530

    change

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -77,7 +77,7 @@ export const updateUser = expressAsyncHandler(async (req, res) => {
       {
         userName,
         email,
-        image: req.file.path,
+        // image: req.file.path,
       },
       { new: true }
     );
